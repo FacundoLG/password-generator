51b28a2801beb3f12c32905edd25d35181120c9b
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-04-04 22:01:44 -0300

    symbols

--- password-generator.py
+++ password-generator.py
@@ -1,6 +1,7 @@
 import random
 import string
 lower_letters = string.ascii_lowercase
+symbols = string.punctuation
 def generate_letters(length: int):
     letters = ""
     for _ in range(length):
@@ -12,9 +13,18 @@ def generate_letters(length: int):
             letters += lower_letters[random_index].upper()
     return letters
 
+def generate_number(max_value: int = 10000):
+    return str(random.randint(0,max_value))
+def random_symbol():
+    return symbols[random.randint(0, len(symbols) - 1)]
+
 def generate_password():
     part1 = generate_letters(10)    
-    print(part1)
+    symbol = random_symbol()    
+    part2 = generate_number(1000000000)
+    symbol2 = random_symbol()
+    part3 = generate_letters(20)
+    print(part1 +symbol+ part2+ symbol2+ part3)
 
 def run():
     generate_password()
